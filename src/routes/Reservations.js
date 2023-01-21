a20d164d5d1bd09ba8dc7823bbef008cf12a5e83
Yusuff Ridwan <wemzain@gmail.com>
2023-01-21 21:26:39 +0100

    added the BookingForm component

--- src/routes/Reservations.js
+++ src/routes/Reservations.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import HeaderElement from '../components/HeaderElement';
-import Footer from '../components/Footer';
+import React from "react";
+import HeaderElement from "../components/HeaderElement";
+import Footer from "../components/Footer";
+// import BookingForm from "../components/BookingForm";
 
 const Reservations = () => {
   return (
     <div>
       <HeaderElement />
-      <h1 style={{textAlign: "center", paddingTop: "15%", height: "90vh"}}>Reservations</h1>
+      <h1 style={{ textAlign: "center", paddingTop: "15%", height: "90vh" }}>
+        Reservations
+      </h1>
+      {/* <BookingForm /> */}
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Reservations
+export default Reservations;
